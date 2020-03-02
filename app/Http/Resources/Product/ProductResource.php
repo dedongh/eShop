9cc3786278c4cbf7d8fb32aadf59c19aa4e4827a
KAY <slimshades68@gmail.com>
2020-03-02 15:45:00 +0000

    Get all products Get product reviews get single product

--- app/Http/Resources/Product/ProductResource.php
+++ app/Http/Resources/Product/ProductResource.php
@@ -6,21 +6,30 @@ use Illuminate\Http\Resources\Json\Resource;
 
 class ProductResource extends Resource
 {
+
+
     /**
      * Transform the resource into an array.
      *
-     * @param  \Illuminate\Http\Request  $request
+     * @param \Illuminate\Http\Request $request
      * @return array
      */
     public function toArray($request)
     {
         return [
-            'type'=> 'product',
-            'id'=> $this->id,
-            'name'=>$this->name,
-            'description'=>$this->details,
-            'quantity'=>$this->quantity,
-            'image'=>$this->image
+            'type' => 'product',
+            'id' => $this->id,
+            'attributes' => [
+                'name' => $this->name,
+                'description' => $this->details,
+                'quantity' => $this->quantity,
+                'rating' => $this->reviews->count() > 0 ?
+                    round($this->reviews->sum('rating') / $this->reviews->count()) : 0,
+                'image' => $this->image,
+            ],
+            'relationships' => [
+                'reviews' => route('reviews.index', $this->id)
+            ],
         ];
     }
 }
