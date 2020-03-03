92e3dd09426a580c087569d47409ba33b99364f2
KAY <slimshades68@gmail.com>
2020-03-03 15:50:31 +0000

    Add Review

--- app/Model/Review.php
+++ app/Model/Review.php
@@ -6,6 +6,9 @@ use Illuminate\Database\Eloquent\Model;
 
 class Review extends Model
 {
+    protected $fillable = [
+        'customer','review','rating'
+    ];
     //
     public function product()
     {
